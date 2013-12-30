e5674882ec6a661f86b79f1aaffe6f1aa61ff40e
Mike Simpson <mike.simpson@scimedsolutions.com>
2013-12-29 21:30:49 -0500

    Update packages

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "65ae93029a583d69a3781b26044601e85e2d32be8f525988e196ba2cb644ce6a" default)))
  '(org-agenda-files (quote ("~/org/time.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
