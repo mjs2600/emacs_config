628236ac6632b59060281991a09b7556094205c2
Mike Simpson <mike.simpson@scimedsolutions.com>
2014-02-28 15:44:56 -0500

    Update packages

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("9c26d896b2668f212f39f5b0206c5e3f0ac301611ced8a6f74afe4ee9c7e6311" "8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "65ae93029a583d69a3781b26044601e85e2d32be8f525988e196ba2cb644ce6a" default)))
+ '(custom-safe-themes (quote ("a3d519ee30c0aa4b45a277ae41c4fa1ae80e52f04098a2654979b1ab859ab0bf" "73fe242ddbaf2b985689e6ec12e29fab2ecd59f765453ad0e93bc502e6e478d6" "9c26d896b2668f212f39f5b0206c5e3f0ac301611ced8a6f74afe4ee9c7e6311" "8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "65ae93029a583d69a3781b26044601e85e2d32be8f525988e196ba2cb644ce6a" default)))
  '(org-agenda-files (quote ("~/org/time.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -14,3 +14,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
